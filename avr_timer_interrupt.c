3e4549c3c1d0c1dcb919a456060847324999c1c5
Alexandr Larionov <alarionov93@yandex.ru>
2017-04-11 23:45:55 +0500

    fixing errors, code not working

--- avr_timer_interrupt.c
+++ avr_timer_interrupt.c
@@ -1,4 +1,4 @@
-#define F_CPU 12000000UL
+#define F_CPU 16000000UL
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -27,29 +27,29 @@ inline void setup() {
   TCCR1B |= (1 << CS10);
   TIMSK1 |= (1 << TOIE1);
   //8 data bits
-  UCSR0C |= (1 << UCSZ01);
-  UCSR0C |= (1 << UCSZ00);
-  //enable recieve and transmit
-  UCSR0B |= (1 << RXEN0);
-  UCSR0B |= (1 << TXEN0);
-  //enable interrupt on recieving
-  UCSR0B |= (1 << RXCIE0);
+  // UCSR0C |= (1 << UCSZ01);
+  // UCSR0C |= (1 << UCSZ00);
+  // //enable recieve and transmit
+  // UCSR0B |= (1 << RXEN0);
+  // UCSR0B |= (1 << TXEN0);
+  // //enable interrupt on recieving
+  // UCSR0B |= (1 << RXCIE0);
 
-  UBRR0H = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
-  UBRR0L = BAUD_PRESCALE; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
+  // UBRR0H = (BAUD_PRESCALE >> 8); // Load upper 8-bits of the baud rate value into the high byte of the UBRR register
+  // UBRR0L = BAUD_PRESCALE; // Load lower 8-bits of the baud rate value into the low byte of the UBRR register
 
   sei();
 }
 
-ISR(USART_RX_vect) {
-    // // _delay_ms(2);
-    cli();
-    incom = UDR0;
-    PORTB |= (1 << GREEN_LED);
-    _delay_ms(20);
-    PORTB &= ~(1 << GREEN_LED);
-    // reti();
-}
+// ISR(USART_RX_vect) {
+//     // // _delay_ms(2);
+//     cli();
+//     incom = UDR0;
+//     PORTB |= (1 << GREEN_LED);
+//     _delay_ms(20);
+//     PORTB &= ~(1 << GREEN_LED);
+//     // reti();
+// }
 
 void main(void) {
 	setup();
